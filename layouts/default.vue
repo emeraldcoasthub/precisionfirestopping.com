fef6f93c8fc705c889c728a8cfb130fe3754ae3c
Patrick Black Jr <info@patrickblackjr.com>
2019-02-07 15:50:56 -0600

    feat(footer): add footer component

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,18 +2,18 @@
   <div>
     <NavBar/>
     <nuxt/>
+    <Footer/>
   </div>
 </template>
 
-<style>
-</style>
-
 <script>
-import NavBar from '@/components/NavBar.vue'
+import NavBar from '@/components/NavBar.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   components: {
-    NavBar
+    NavBar,
+    Footer
   }
 }
 </script>
