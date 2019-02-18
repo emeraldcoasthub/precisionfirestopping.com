232e079a179ef2450c9b91e7ebf1c87527a8f71e
Patrick Black Jr <info@patrickblackjr.com>
2019-02-18 14:21:53 -0600

    Fix responsive issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,12 +9,10 @@ module.exports = {
   head: {
     title: 'Firestopping Solutions',
     titleTemplate: '%s • Precision Firestopping',
-    metaInfo: {
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' }
-      ],
-    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
